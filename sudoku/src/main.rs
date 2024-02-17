e3074589411a76e96f4fa48e19f238b98fa14e20
EasChen70 <eason_chen@uri.edu>
2024-02-16 23:01:58 -0500

    testing

--- sudoku/src/main.rs
+++ sudoku/src/main.rs
@@ -1,20 +1,30 @@
 use csc411_image::{Read, GrayImage};
 use array2::Array2;
+use array2::IterRowMajor;
+use array2::IterColumnMajor;
 use std::env;
 
 
 fn main() {
     let input = env::args().nth(1);
     let img = GrayImage::read(input.as_deref()).unwrap();
+    //create 1d vector of pixels from grayimage
     let mut hold = vec![];
     for x in img.pixels.into_iter(){
         hold.push(x.value);
     }
+    //gives data from hold to array2
     let array = Array2::with_data(img.width.try_into().unwrap(),img.height.try_into().unwrap(),hold);
-   array.printwidth();
-   let check = array.from_row_major();
-   for row in &check {
-    println!("{:?}", row);
-}
+    
+    //makes 2d vector
+    let sudoku = array.from_row_major();
+    for row in &sudoku {
+        println!("{:?}", row);
+    }
+    let result = sudoku.iter_row_major();
+    println!("{:?}", result);
+    let result2 = sudoku.iter_column_major();
+    println!("{:?}", result2);
 
+    
 }
