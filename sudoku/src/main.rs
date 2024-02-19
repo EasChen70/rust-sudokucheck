a7a6149ceb57007893e1dddd0db520b0758ece7c
EasChen70 <eason_chen@uri.edu>
2024-02-19 00:02:16 -0500

    normalized value/ error checks

--- sudoku/src/main.rs
+++ sudoku/src/main.rs
@@ -6,12 +6,36 @@ use std::env;
 
 
 fn main() {
-    let input = env::args().nth(1);
-    let img = GrayImage::read(input.as_deref()).unwrap();
+    let input = match env::args().nth(1){
+        Some(input) => input,
+        None => {
+            eprintln!("Invalid File/No File Found");
+            std::process::exit(1);
+        }
+    };
+    
+    let img = match GrayImage::read(Some(&input)){
+        Ok(img) => img,
+        Err(err) => {
+            eprintln!("Error with Image {}", err);
+            std::process::exit(1);
+        }
+    };
+
+    //Check image dimensions
+    let width = img.width;
+    let height = img.height;
+    if width != 9 || height != 9{
+        eprintln!("Not valid dimensions for sudoku puzzle");
+        std::process::exit(1);
+    }
+
     //create 1d vector of pixels from grayimage
     let mut hold = vec![];
+    
     for x in img.pixels.into_iter(){
-        hold.push(x.value);
+        let normalized_value = (x.value as f32/ 28.33).round() as u8;
+        hold.push(normalized_value);
     }
     //gives data from hold to array2
     let array = Array2::with_data(img.width.try_into().unwrap(),img.height.try_into().unwrap(),hold);
@@ -26,5 +50,4 @@ fn main() {
     let result2 = sudoku.iter_column_major();
     println!("{:?}", result2);
 
-    
 }
